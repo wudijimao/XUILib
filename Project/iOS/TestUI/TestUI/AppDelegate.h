7ac9812b5c4612b7acf9870c89338157a3f9c262
ximiao <wudijimao@qq.com>
2016-03-17 16:05:31 +0800

    新增ios项目

---
+++ Project/iOS/TestUI/TestUI/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestUI
+//
+//  Created by ximiao on 16/3/17.
+//  Copyright © 2016年 wudijimao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Project/iOS/TestUI/TestUI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestUI
+//
+//  Created by ximiao on 16/3/17.
+//  Copyright © 2016年 wudijimao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
